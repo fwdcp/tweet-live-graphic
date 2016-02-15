0caecc18544b51c2fd99ae4d5d8f20236274d753
CoolAcid <jakendall@gmail.com>
2016-02-15 13:03:43 -0500

    Make functional on NodeCG 0.7.x

--- dashboard/panel.js
+++ dashboard/panel.js
@@ -1,49 +1,52 @@
-$('#tweetfeature-change-tweet').click(function() {
-    $.getJSON('https://api.twitter.com/1/statuses/oembed.json?callback=?', {
-        url: $('#tweetfeature-tweet').val(),
-        maxwidth: 450,
-        align: 'center'
-    }, function(data) {
-        if (data.html) {
-            nodecg.sendMessage('tweetfeatureChangeTweet', {
-                tweet: data.html
-            });
-        }
+$(function() {
+
+    $('#tweetfeature-change-tweet').click(function() {
+        $.getJSON('https://api.twitter.com/1/statuses/oembed.json?callback=?', {
+            url: $('#tweetfeature-tweet').val(),
+            maxwidth: 450,
+            align: 'center'
+        }, function(data) {
+            if (data.html) {
+                nodecg.sendMessage('tweetfeatureChangeTweet', {
+                    tweet: data.html
+                });
+            }
+        });
     });
-});
 
-$('#tweetfeature-show').click(function() {
-    $.getJSON('https://api.twitter.com/1/statuses/oembed.json?callback=?', {
-        url: $('#tweetfeature-tweet').val(),
-        maxwidth: 450,
-        align: 'center'
-    }, function(data) {
-        if (data.html) {
-            nodecg.sendMessage('tweetfeatureChangeTweet', {
-                tweet: data.html
-            });
-            nodecg.sendMessage('tweetfeatureShow');
-        }
+    $('#tweetfeature-show').click(function() {
+        $.getJSON('https://api.twitter.com/1/statuses/oembed.json?callback=?', {
+            url: $('#tweetfeature-tweet').val(),
+            maxwidth: 450,
+            align: 'center'
+        }, function(data) {
+            if (data.html) {
+                nodecg.sendMessage('tweetfeatureChangeTweet', {
+                    tweet: data.html
+                });
+                nodecg.sendMessage('tweetfeatureShow');
+            }
+        });
     });
-});
 
-$('#tweetfeature-hide').click(function() {
-    nodecg.sendMessage('tweetfeatureHide');
-});
+    $('#tweetfeature-hide').click(function() {
+        nodecg.sendMessage('tweetfeatureHide');
+    });
 
-$('#tweetfeature-display-timed').click(function() {
-    $.getJSON('https://api.twitter.com/1/statuses/oembed.json?callback=?', {
-        url: $('#tweetfeature-tweet').val(),
-        maxwidth: 450,
-        align: 'center'
-    }, function(data) {
-        if (data.html) {
-            nodecg.sendMessage('tweetfeatureChangeTweet', {
-                tweet: data.html
-            });
-            nodecg.sendMessage('tweetfeatureDisplayTimed', {
-                time: $('#tweetfeature-time').val()
-            });
-        }
+    $('#tweetfeature-display-timed').click(function() {
+        $.getJSON('https://api.twitter.com/1/statuses/oembed.json?callback=?', {
+            url: $('#tweetfeature-tweet').val(),
+            maxwidth: 450,
+            align: 'center'
+        }, function(data) {
+            if (data.html) {
+                nodecg.sendMessage('tweetfeatureChangeTweet', {
+                    tweet: data.html
+                });
+                nodecg.sendMessage('tweetfeatureDisplayTimed', {
+                    time: $('#tweetfeature-time').val()
+                });
+            }
+        });
     });
 });
